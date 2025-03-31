1636e37c607b14d9e440b8ea87a5697d91bdebfa
FilippoAmenta <amentafilippo2001@gmail.com>
2025-03-31 15:50:21 +0200

    -

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ DEBUG = env.bool("DEBUG", default=False)
 
 SECRET_KEY = env.str('SECRET_KEY', default='django-insecure-!@#qwertyuiop')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 INSTALLED_APPS = [
     'mptt',
