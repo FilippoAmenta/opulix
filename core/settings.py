fe333ebb6154df7dc3c02f5d111d73d52d110ed6
FilippoAmenta <amentafilippo2001@gmail.com>
2025-03-31 15:16:40 +0200

    -

--- core/settings.py
+++ core/settings.py
@@ -97,6 +97,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CURRENCY_CHOICES = [('EUR', '€')]
